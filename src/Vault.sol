3833bb00c3a32d5a1561be56b0e980460eba9fed
“Kyle <KyleMcodes@gmail.com>
2026-01-09 20:50:54 -0800

    ERC from scratch

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,7 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; // Importing IERC20 interface from OpenZeppelin
+
+contract SkywalkerToken {
+    string public name = "Skywalker Token";
+    string public symbol = "SKY";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor (uint256 _initialSupply) {
+        totalSupply = _initialSupply *10 ** uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool success) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool success) {
+        allowance[msg.sender][spender] = amount; 
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool success) {
+        require(balanceOf[sender] >= amount, "Insufficient balance");
+        require(allowance[sender][msg.sender] >= amount, "Allowance exceeded");
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        allowance[sender][msg.sender] -= amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+}
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
 
 
 // Vault contract to handle deposits and withdrawals of an ERC20 token
@@ -10,8 +56,8 @@ contract Vault {
 
     mapping(address => uint256) public balances;
 
-    constructor(address _tokenAdress) {
-        token = IERC20 (_tokenAdress);
+    constructor(address _tokenAddress) {
+        token = IERC20 (_tokenAddress);
     }
     // Deposit function to allow users to deposit tokens into the vault
     function deposit(uint256 amount) external {
