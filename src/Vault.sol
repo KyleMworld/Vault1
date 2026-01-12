8075492b8308f341bb3aa68bdea4a3d59babf1d5
“Kyle <KyleMcodes@gmail.com>
2026-01-12 12:29:54 -0800

    Formatting to ERC-20 standards - constants, immutable decimals, and past-tense events

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,33 +3,33 @@ pragma solidity ^0.8.18;
 
 
 contract SkywalkerToken {
-    string public name = "Skywalker Token";
-    string public symbol = "SKY";
-    uint8 public decimals = 18;
+    string constant public name = "Skywalker Token";
+    string constant public symbol = "SKY";
+    uint8 immutable public i_decimals = 18;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
     
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfered(address indexed from, address indexed to, uint256 value);
+    event Approved(address indexed owner, address indexed spender, uint256 value);
 
     constructor (uint256 _initialSupply) {
-        totalSupply = _initialSupply *10 ** uint256(decimals);
+        totalSupply = _initialSupply * 10 ** uint256(i_decimals);
         balanceOf[msg.sender] = totalSupply;
-        emit Transfer(address(0), msg.sender, totalSupply);
+        emit Transfered(address(0), msg.sender, totalSupply);
     }
 
     function transfer(address recipient, uint256 amount) public returns (bool success) {
         require(balanceOf[msg.sender] >= amount, "Insufficient balance");
         balanceOf[msg.sender] -= amount;
         balanceOf[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
+        emit Transfered(msg.sender, recipient, amount);
         return true;
     }
 
     function approve(address spender, uint256 amount) public returns (bool success) {
         allowance[msg.sender][spender] = amount; 
-        emit Approval(msg.sender, spender, amount);
+        emit Approved(msg.sender, spender, amount);
         return true;
     }
 
@@ -39,7 +39,7 @@ contract SkywalkerToken {
         balanceOf[sender] -= amount;
         balanceOf[recipient] += amount;
         allowance[sender][msg.sender] -= amount;
-        emit Transfer(sender, recipient, amount);
+        emit Transfered(sender, recipient, amount);
         return true;
     }
 }
